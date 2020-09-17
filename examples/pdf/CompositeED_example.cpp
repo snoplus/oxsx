dc50cc2ad74d14dae83542e6f96e852b97c9b87f
dcookman <daniel.cookman@physics.ox.ac.uk>
2020-09-17 16:58:34 +0100

    Convert CompositePdf.cpp --> CompositeED_example.cpp

--- examples/pdf/CompositeED_example.cpp
+++ examples/pdf/CompositeED_example.cpp
@@ -1,11 +1,11 @@
 /*
   Pdfs containing independent components for example:
         P(E, r, t) = P(E) * P(r) * P(t)
-  are delt with using the CompositePdf class. Just create the pdfs 
+  are dealt with using the CompositeED class. Just create the pdfs 
   independently and then multiply them together to get a composite pdf.
 
   If you've correctly set the data representation of all the constituents
-  you can call compositePdf.Probability(anEvent) and each of the consitent
+  you can call compositeED.Probability(anEvent) and each of the consitent
   pdfs will work out what to do!
 
   In this example we combine a 2D gaussian analytic pdf, 
@@ -15,29 +15,29 @@
  */
 
 #include <Gaussian.h>
-#include <AnalyticPdf.h>
-#include <CompositePdf.h>
+#include <AnalyticED.h>
+#include <CompositeED.h>
 
 int main(){
     // Build the constituent parts independently
     Gaussian gaus2d(2); // normal parameters by default
-    AnalyticPdf pdf2d(&gaus2d);
+    AnalyticED pdf2d("analytic2D", &gaus2d);
     // 2d pdf will look at observables 1 and 3
     // you can address these by names if they come from a data set
     std::vector<size_t> indices;
     indices.push_back(1);
     indices.push_back(3);
 
-    pdf2d.SetDataRep(DataRepresentation(indices));
+    pdf2d.SetObservables(ObsSet(indices));
 
     Gaussian gaus1d(1);
-    AnalyticPdf pdf1d(&gaus1d);
+    AnalyticED pdf1d("analytic1D", &gaus1d);
     // 1d pdf will look at observable 4
-    pdf1d.SetDataRep(4);
+    pdf1d.SetObservables(ObsSet(4));
 
 
     // Now we combine them into a single composite pdf
-    CompositePdf combinedPdf = pdf2d * pdf1d;
+    CompositeED combinedPdf = pdf2d * pdf1d;
     
 
     std::cout << "From a "<< pdf2d.GetNDims() << "D pdf\n"
