83fa9567db5b14ca91b128473633d7802963a034
dcookman <daniel.cookman@physics.ox.ac.uk>
2021-01-23 17:22:12 +0000

    Correct Histogram::AddPadding() signed/unsigned warning

--- src/histogram/Histogram.cpp
+++ src/histogram/Histogram.cpp
@@ -259,7 +259,7 @@ Histogram::GetAxisNames() const{
 void
 Histogram::AddPadding(double padding_){
   std::vector<double> newBinContents;
-  for(int i =0; i<fBinContents.size();i++){
+  for(size_t i = 0; i<fBinContents.size(); i++){
     if(fBinContents.at(i)==0)
       newBinContents.push_back(padding_);
     else
