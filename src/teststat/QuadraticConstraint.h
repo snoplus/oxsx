f95f59c90c6b4ca87bd9d5037b1d1ca737cbbad4
dcookman <daniel.cookman@physics.ox.ac.uk>
2021-06-28 10:54:37 +0100

    Add asym functionality to QuadraticConstraint

--- src/teststat/QuadraticConstraint.h
+++ src/teststat/QuadraticConstraint.h
@@ -7,16 +7,25 @@
 
 class QuadraticConstraint{
  public:
- QuadraticConstraint(){}
- QuadraticConstraint(double mean_, double width_) : 
-    fMean(mean_), fWidth(width_) {}
+    QuadraticConstraint(){}
+    // Symmetric constraint constructor
+    QuadraticConstraint(double mean_, double width_) : 
+        fMean(mean_), fWidth(width_), fWidth_lo(0), is_asym(false) {}
+    // Asymmetric constraint constructor
+    QuadraticConstraint(double mean_, double width_hi, double width_lo) :
+        fMean(mean_), fWidth(width_hi), fWidth_lo(width_lo), is_asym(true) {}
 
     double Evaluate(double val_) const {
-        return (val_ - fMean) * (val_ - fMean) / (2 * fWidth * fWidth);
+        double sigma;
+        if (is_asym) { sigma = (val_ >= fMean) ? fWidth : fWidth_lo; }
+        else { sigma = fWidth; }
+        return (val_ - fMean) * (val_ - fMean) / (2 * sigma * sigma);
     }
     
  private:
     double fMean;
     double fWidth;
+    double fWidth_lo;
+    bool is_asym;
 };
 #endif
