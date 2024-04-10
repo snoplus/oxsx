6e976cc9686125ca9b40b6b0679afd36913132b1
parkerw <william.parker@physics.ox.ac.uk>
2024-04-10 15:35:57 +0100

    don't have bin centre integral in varying cdf (defeats point of varying cdf no?)

--- src/function/VaryingCDF.h
+++ src/function/VaryingCDF.h
@@ -43,51 +43,47 @@
 
 class VaryingCDF : public ConditionalPDF{
  public:
-  VaryingCDF(const std::string&);
-  ~VaryingCDF();
+    VaryingCDF(const std::string&);
+    ~VaryingCDF();
 
-  VaryingCDF(const VaryingCDF& copy_);
+    VaryingCDF(const VaryingCDF& copy_);
 
-  VaryingCDF& operator=(const VaryingCDF& other_);
+    VaryingCDF& operator=(const VaryingCDF& other_);
 
-  double ConditionalProbability(const std::vector<double>& x_, 
-                                const std::vector<double>& x2_);
-  double Integral(const std::vector<double>& mins_, 
-                  const std::vector<double>& maxs_,
-                  const std::vector<double>& x2_) const;
-  double Integral(const std::vector<double>& mins_, 
-                  const std::vector<double>& maxs_,
-                  const std::vector<double>& x2_,
-                  const double& bincentre) const;
+    double ConditionalProbability(const std::vector<double>& x_, 
+            const std::vector<double>& x2_);
+    double Integral(const std::vector<double>& mins_, 
+                    const std::vector<double>& maxs_,
+                    const std::vector<double>& x2_) const;
 
-  std::vector<double> Sample(const std::vector<double>&) const;
+    std::vector<double> Sample(const std::vector<double>&) const;
 
-  ConditionalPDF* Clone() const;
+    ConditionalPDF* Clone() const;
 
-  void SetKernel(ConditionalPDF* PDF_);
-  void SetKernel(PDF* PDF_);
-  void SetDependance(const std::string& paraName_, const Function* func_);
+    void SetKernel(ConditionalPDF* PDF_);
+    void SetKernel(PDF* PDF_);
+    void SetDependance(const std::string& paraName_, const Function* func_);
 
-  ParameterDict SetupParameters(const std::vector<double>& x2_) const;
+    ParameterDict SetupParameters(const std::vector<double>& x2_) const;
 
-  // FitParameter interface.
-  void   SetParameter(const std::string& name_, double value);
-  double GetParameter(const std::string& name_) const;
+    // FitParameter interface.
+    void   SetParameter(const std::string& name_, double value);
+    double GetParameter(const std::string& name_) const;
 
-  void   SetParameters(const ParameterDict&);
-  ParameterDict GetParameters() const;
-  size_t GetParameterCount() const;
+    void   SetParameters(const ParameterDict&);
+    ParameterDict GetParameters() const;
+    size_t GetParameterCount() const;
 
-  std::set<std::string> GetParameterNames() const;
-  void   RenameParameter(const std::string& old_, const std::string& new_);
+    std::set<std::string> GetParameterNames() const;
+    void   RenameParameter(const std::string& old_, const std::string& new_);
 
-  std::string GetName() const;
-  void SetName(const std::string&);
+    std::string GetName() const;
+    void SetName(const std::string&);
 
  private:
-  std::map<std::string, Function*> fFunctions;
-  ConditionalPDF* fCdf;
+    std::map<std::string, Function*> fFunctions;
+    ConditionalPDF* fCdf;
     
-  std::string fName;
+    std::string fName;
 };
 #endif
