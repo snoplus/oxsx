7f0b12d214626a09ed2395ba0d04e25d101bdbc6
parkerw <william.parker@physics.ox.ac.uk>
2024-04-15 17:22:03 +0100

    Adding a scale systematic where the form of the scale can be any function

---
+++ src/systematic/ScaleFunction.h
@@ -0,0 +1,55 @@
+/*****************************************/
+/* A scale function error on an observable */
+/*****************************************/
+#ifndef __OXSX_SCALEFUNCTION__
+#define __OXSX_SCALEFUNCTION__
+#include <Systematic.h>
+#include <string>
+#include <DenseMatrix.h>
+#include <functional>
+
+/*
+* Firstly, create a typdef for a general function that just 
+* takes in a double and the parameter dict, and returns a
+* double.
+*/
+typedef std::function<double(const ParameterDict&,
+                           const double&)> ScaleFunc;
+
+class ScaleFunction : public Systematic{
+ public:
+    ScaleFunction(const std::string& name_) : fScaleFunc(), fParamDict(), fName(name_), fCachedBinMapping(false) {}
+
+    void SetScaleFunction(const ScaleFunc& scale_func,
+                          const std::vector<std::string>& param_names);
+
+    void Construct();
+
+    void   SetParameter(const std::string& name_, double value);
+    double GetParameter(const std::string& name_) const;
+
+    void   SetParameters(const ParameterDict&);
+    ParameterDict GetParameters() const { return fParamDict; }
+    size_t GetParameterCount() const { return fParamDict.size(); }
+
+    std::set<std::string> GetParameterNames() const;
+    void   RenameParameter(const std::string& old_, const std::string& new_);
+
+    std::string GetName() const { return fName; }
+    void SetName(const std::string& name_) { fName = name_; }
+ 
+ private:
+    ScaleFunc          fScaleFunc;
+    ParameterDict      fParamDict;
+    std::string        fName;
+    std::string        fParamName;
+
+    std::vector<size_t> fDistTransBinMapping;
+    DenseMatrix fMappingDistAndTrans;
+    bool fCachedBinMapping;
+
+    void CacheBinMapping();
+    double GetBinContribution(const BinAxis& scaleAxis, double scaledLow,
+                                     double scaledHigh, size_t bin_test);
+};
+#endif
---
+++ test/unit/ScaleFuncSystTest.cpp
@@ -0,0 +1,132 @@
+#include <catch.hpp>
+#include <SystematicManager.h>
+#include <ScaleFunction.h>
+
+double linear_func(const ParameterDict& params, const double& obs_val) {
+    /*
+    * Simple non-trivial function, linear in x:
+    * returns = a*x + b
+    */
+    return params.at("a") * obs_val + params.at("b");
+}
+
+TEST_CASE("Simple ScaleFunction systematic on 1d PDF"){
+    AxisCollection axes;
+    axes.AddAxis(BinAxis("axis0", 0, 5, 5));
+    std::vector<std::string> observables;
+    observables.push_back("obs0");
+
+    BinnedED pdf1("pdf1", axes);
+    pdf1.SetBinContent(0, 0);
+    pdf1.SetBinContent(1, 10);
+    pdf1.SetBinContent(2, 0);
+    pdf1.SetBinContent(3, 0);
+    pdf1.SetBinContent(4, 0);
+    pdf1.SetObservables(observables);
+
+    const std::vector<std::string> scale_param_names {"a", "b"};
+    const std::set<std::string> scale_param_names_set {"a", "b"};
+
+    ScaleFunction* scale_func = new ScaleFunction("scale_func_sys");
+    scale_func->SetScaleFunction(linear_func, scale_param_names);
+    const auto params = ParameterDict({{"a", 2}, {"b", 0}});
+    scale_func->SetParameters(params);
+    scale_func->SetAxes(axes);
+    scale_func->SetTransformationObs(observables);
+    scale_func->SetDistributionObs(observables);
+    scale_func->Construct();
+
+    SECTION("Check systematic name") {
+        REQUIRE( scale_func->GetName() == "scale_func_sys" );
+    }
+
+    SECTION("Check observables used") {
+        REQUIRE( scale_func->GetDistributionObs() == observables );
+        REQUIRE( scale_func->GetTransformationObs() == observables );
+    }
+
+    SECTION("Check axes") {
+        REQUIRE( scale_func->GetAxes() == axes );
+    }
+
+    SECTION("Check argument information") {
+        REQUIRE( scale_func->GetParameterCount() == 2 );
+        REQUIRE( scale_func->GetParameterNames() == scale_param_names_set );
+        REQUIRE( scale_func->GetParameter("a") == 2 );
+        REQUIRE( scale_func->GetParameter("b") == 0 );
+        REQUIRE( scale_func->GetParameters() == params );
+    }
+
+    SystematicManager man;
+    Systematic* syst1;
+    syst1 = scale_func;
+    man.Add(syst1);
+    man.AddDist(pdf1,"");
+    man.Construct();
+
+    std::vector<BinnedED> pdfs;
+    pdfs.push_back(pdf1);
+    std::vector<BinnedED> OrignalPdfs(pdfs);
+
+    man.DistortEDs(OrignalPdfs,pdfs);
+
+    std::vector<double> modifiedObs = pdfs.at(0).GetBinContents();
+    std::vector<double> correctVals;
+    correctVals.push_back(0);
+    correctVals.push_back(0);
+    correctVals.push_back(5);
+    correctVals.push_back(5);
+    correctVals.push_back(0);
+
+    REQUIRE(modifiedObs == correctVals);
+}
+
+TEST_CASE("Another simple ScaleFunction systematic on 2d PDF"){
+    AxisCollection axes;
+    axes.AddAxis(BinAxis("axis0", 0, 5, 5));
+    axes.AddAxis(BinAxis("axis1", 1, 3, 2));
+    std::vector<std::string> observables;
+    observables.push_back("obs0");
+    observables.push_back("obs1");
+
+    BinnedED pdf1("pdf1", axes);
+    pdf1.SetBinContent(axes.FlattenIndices({0,0}), 12);
+    pdf1.SetBinContent(axes.FlattenIndices({1,1}), 6);
+    pdf1.SetObservables(observables);
+
+    const std::vector<std::string> scale_param_names {"a", "b"};
+    const std::set<std::string> scale_param_names_set {"a", "b"};
+
+    ScaleFunction* scale_func = new ScaleFunction("scale_func_sys");
+    scale_func->SetScaleFunction(linear_func, scale_param_names);
+    const auto params = ParameterDict({{"a", 3}, {"b", 1}});
+    scale_func->SetParameters(params);
+    scale_func->SetAxes(axes);
+    scale_func->SetTransformationObs({"obs0"});
+    scale_func->SetDistributionObs(observables);
+    scale_func->Construct();
+
+    SystematicManager man;
+    Systematic* syst1;
+    syst1 = scale_func;
+    man.Add(syst1);
+    man.AddDist(pdf1,"");
+    man.Construct();
+
+    std::vector<BinnedED> pdfs;
+    pdfs.push_back(pdf1);
+    std::vector<BinnedED> OrignalPdfs(pdfs);
+
+    man.DistortEDs(OrignalPdfs,pdfs);
+
+    std::vector<double> modifiedObs = pdfs.at(0).GetBinContents();
+    std::vector<double> correctVals(10,0);
+    correctVals[axes.FlattenIndices({1,0})] = 4;
+    correctVals[axes.FlattenIndices({2,0})] = 4;
+    correctVals[axes.FlattenIndices({3,0})] = 4;
+    correctVals[axes.FlattenIndices({4,1})] = 2;
+    
+    for (size_t i=0; i<modifiedObs.size(); i++) {
+        REQUIRE(modifiedObs.at(i) == Approx(correctVals.at(i)));
+    }
+}
