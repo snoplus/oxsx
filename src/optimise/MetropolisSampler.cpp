29b12ffd4fc1305b7907e76da1d5f62fa287ef34
parkerw <william.parker@physics.ox.ac.uk>
2024-09-14 01:17:36 +0100

    allow parameters to be fixed in Metropolis Sampler

--- src/optimise/MetropolisSampler.cpp
+++ src/optimise/MetropolisSampler.cpp
@@ -12,12 +12,26 @@ void MetropolisSampler::SetSigmas(const ParameterDict &sig_)
     fSigmas = sig_;
 }
 
+void MetropolisSampler::Fix(const std::string &name_)
+{
+    fFixedParameters.insert(name_);
+}
+
+void MetropolisSampler::Release(const std::string &name_)
+{
+    fFixedParameters.erase(name_);
+}
+
 ParameterDict
 MetropolisSampler::Draw(const ParameterDict &current_)
 {
     ParameterDict newStep;
     for (ParameterDict::const_iterator it = current_.begin(); it != current_.end(); ++it)
     {
+      // If the parameter is fixed, don't change it
+      if( fFixedParameters.count(it->first) )
+	newStep[it->first] = it->second;
+      else
         newStep[it->first] = it->second + Rand::Gaus(0, fSigmas.at(it->first));
     }
     return newStep;
--- src/optimise/MetropolisSampler.h
+++ src/optimise/MetropolisSampler.h
@@ -2,6 +2,7 @@
 #define __OXSX_METROPOLIS_SAMPLER__
 #include <MCSampler.h>
 #include <ParameterDict.h>
+#include <set>
 
 class MetropolisSampler : public MCSampler
 {
@@ -9,10 +10,14 @@ public:
    const ParameterDict &GetSigmas() const;
    void SetSigmas(const ParameterDict &);
 
+   void Fix(const std::string &param_);
+   void Release(const std::string &param_);
+
    ParameterDict Draw(const ParameterDict &current_);
    inline double CorrectAccParam() { return 0; }
 
 private:
    ParameterDict fSigmas;
+   std::set<std::string> fFixedParameters;
 };
 #endif
