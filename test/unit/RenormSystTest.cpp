881bebfeda405b9004ca1f834683348d33fe79cf
parkerw <william.parker@physics.ox.ac.uk>
2025-10-16 17:13:36 +0100

    and a unit test

---
+++ test/unit/RenormSystTest.cpp
@@ -0,0 +1,136 @@
+#include <catch2/catch_all.hpp>
+#include <catch2/catch_approx.hpp>
+#include <SystematicManager.h>
+#include <Renorm.h>
+
+TEST_CASE("Simple renormalisation systematic on 1d PDF")
+{
+    AxisCollection axes;
+    axes.AddAxis(BinAxis("axis0", 0, 5, 5));
+    std::vector<std::string> observables;
+    observables.push_back("obs0");
+
+    BinnedED pdf1("pdf1", axes);
+    pdf1.SetBinContent(0, 5);
+    pdf1.SetBinContent(1, 12);
+    pdf1.SetBinContent(2, 10);
+    pdf1.SetBinContent(3, 8);
+    pdf1.SetBinContent(4, 3);
+    pdf1.SetObservables(observables);
+
+    Renorm *renorm = new Renorm("renorm");
+    renorm->SetScaleFactor(2.0);
+    renorm->SetAxes(axes);
+    renorm->Construct();
+
+    SystematicManager man;
+    Systematic *syst1;
+    syst1 = renorm;
+    man.Add(syst1);
+    man.AddDist(pdf1, "");
+    man.Construct();
+
+    std::vector<BinnedED> pdfs;
+    pdfs.push_back(pdf1);
+    std::vector<BinnedED> OrignalPdfs(pdfs);
+
+    man.DistortEDs(OrignalPdfs, pdfs);
+
+    std::vector<double> modifiedObs = pdfs.at(0).GetBinContents();
+    std::vector<double> correctVals;
+    correctVals.push_back(10);
+    correctVals.push_back(24);
+    correctVals.push_back(20);
+    correctVals.push_back(16);
+    correctVals.push_back(6);
+
+    REQUIRE(modifiedObs == correctVals);
+}
+
+TEST_CASE("Another simple renormalisation systematic on 2d PDF")
+{
+    AxisCollection axes;
+    axes.AddAxis(BinAxis("axis0", 0, 5, 5));
+    axes.AddAxis(BinAxis("axis1", 1, 3, 2));
+    std::vector<std::string> observables;
+    observables.push_back("obs0");
+    observables.push_back("obs1");
+
+    BinnedED pdf1("pdf1", axes);
+    pdf1.SetBinContent(axes.FlattenIndices({1, 0}), 11);
+    pdf1.SetBinContent(axes.FlattenIndices({2, 1}), 22);
+    pdf1.SetObservables(observables);
+
+    Renorm *renorm = new Renorm("renorm");
+    renorm->SetScaleFactor(1.10);
+    renorm->SetAxes(axes);
+    renorm->Construct();
+
+    SystematicManager man;
+    Systematic *syst1;
+    syst1 = renorm;
+    man.Add(syst1);
+    man.AddDist(pdf1, "");
+    man.Construct();
+
+    std::vector<BinnedED> pdfs;
+    pdfs.push_back(pdf1);
+    std::vector<BinnedED> OrignalPdfs(pdfs);
+
+    man.DistortEDs(OrignalPdfs, pdfs);
+
+    std::vector<double> modifiedObs = pdfs.at(0).GetBinContents();
+    std::vector<double> correctVals(10, 0);
+    correctVals[axes.FlattenIndices({1, 0})] = 12.1;
+    correctVals[axes.FlattenIndices({2, 1})] = 24.2;
+
+    for (size_t i = 0; i < modifiedObs.size(); i++)
+    {
+        REQUIRE(modifiedObs.at(i) == Catch::Approx(correctVals.at(i)));
+    }
+}
+
+TEST_CASE("Another simple renormalisation systematic on 3d PDF")
+{
+    AxisCollection axes;
+    axes.AddAxis(BinAxis("axis0", 0, 5, 5));
+    axes.AddAxis(BinAxis("axis1", 1, 3, 2));
+    axes.AddAxis(BinAxis("axis2", 1, 13, 12));
+    std::vector<std::string> observables;
+    observables.push_back("obs0");
+    observables.push_back("obs1");
+    observables.push_back("obs2");
+
+    BinnedED pdf1("pdf1", axes);
+    pdf1.SetBinContent(axes.FlattenIndices({1, 0, 5}), 11);
+    pdf1.SetBinContent(axes.FlattenIndices({2, 1, 3}), 22);
+    pdf1.SetObservables(observables);
+
+    Renorm *renorm = new Renorm("renorm");
+    renorm->SetScaleFactor(0.5);
+    renorm->SetAxes(axes);
+    renorm->Construct();
+
+    SystematicManager man;
+    Systematic *syst1;
+    syst1 = renorm;
+    man.Add(syst1);
+    man.AddDist(pdf1, "");
+    man.Construct();
+
+    std::vector<BinnedED> pdfs;
+    pdfs.push_back(pdf1);
+    std::vector<BinnedED> OrignalPdfs(pdfs);
+
+    man.DistortEDs(OrignalPdfs, pdfs);
+
+    std::vector<double> modifiedObs = pdfs.at(0).GetBinContents();
+    std::vector<double> correctVals(axes.GetNBins(), 0);
+    correctVals[axes.FlattenIndices({1, 0, 5})] = 5.5;
+    correctVals[axes.FlattenIndices({2, 1, 3})] = 11.0;
+
+    for (size_t i = 0; i < modifiedObs.size(); i++)
+    {
+        REQUIRE(modifiedObs.at(i) == Catch::Approx(correctVals.at(i)));
+    }
+}
